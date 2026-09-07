40ee3a8258fd19163888ae3f3d544554d454e475
Andrew Vandermeer <andrewvandermeer@Andrews-MacBook-Air.local>
2026-09-06 18:44:24 -1000

    Added POST request to users

---
+++ backend/internal/models/user.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type User struct {
+	ID           string    `json:"id"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"`
+	FullName     string    `json:"full_name"`
+	Role         string    `json:"role"`
+	CreatedAt    time.Time `json:"created_at"`
+}
